0301f4c0a228e5317dc38c9888993ec2a8582fe7
Hoangvo92 <hoangvod@gmail.com>
2020-08-22 17:24:23 -0500

    add testProject2 with swashbuckle implemented-swagger

---
+++ testProject2/testProject2/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace testProject2
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+
+         /*   config.Routes.MapHttpRoute(
+                name: "ProdApi",
+                routeTemplate: "api/prod/{id}",
+                defaults: new { controller = "products", id = RouteParameter.Optional }
+             );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            */
+        }
+    }
+}
---
+++ testProject2/testProject2/Controllers/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace testProject2.Controllers
+{
+    [RoutePrefix("api/products")]
+    public class ProductsController : ApiController
+    {
+        // GET: api/Products
+        [HttpGet, Route("")]
+        public IEnumerable<string> ReturnAllProducts()
+        {
+            return new string[] { "product1", "product2" };
+        }
+
+        // GET: api/Products/5/orders/cusid
+        [HttpGet, Route("{id}/orders/{cusid}")]
+        public string Get(int id, string cusid)
+        {
+            return "product-orders-" + cusid;
+        }
+
+        // POST: api/Products
+        [HttpPost, Route("")]
+        public void CreateProduct([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        [HttpPut, Route("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete, Route("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
